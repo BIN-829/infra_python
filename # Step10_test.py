5f571260a485f585243c041d38352fab67437472
choyongbin <jyb13579@naver.com>
2026-03-20 14:29:09 +0900

    Step12 추가함

--- # Step10_test.py
+++ # Step10_test.py
@@ -1,12 +1,25 @@
-#Step10_test.py
+# Step10_main.py
 
-from Step10_main import TestClass
+'''
+    현재 파일 즉 Step10_main.py 에서  run 해서 실행을 하면 
+    
+    __name__ 은 "__main__" 이라는 문자열이 들어 있다.
+    
+    따라서 __name__ 을 확인해 보면 해당 파일이 main 으로 실행되었는지 여부를 알수 있다
+
+    다른 곳에서 import 했을때 실행되지 않는 코드 블럭을 만들때 사용한다. 
+'''
+
+print("Step10_main.py 가 실행됩니다")
+print(__name__)
 
 
-if __name__=="__main__":
-    #TestClass() 로 객체(인스턴스) 생성하기
-    a: TestClass=TestClass()
-    print("종료 됩니다")
-
-    if __name__=="__main__":
+# 테스트 용도의 클래스 작성하기 
+class TestClass:
     pass
+
+# 아래의 if 문 블럭은 main 으로 실행되었을때만 실행된다(다른 곳에서 import 했을때는 실행되지 않는다)
+if __name__ == "__main__":
+    print("시작합니다")
+    print("어떤 작업을 하고")
+    print("종료 합니다")
